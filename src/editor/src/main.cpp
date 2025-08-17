9456e179c94f213d4f3d9f99236cc2e700ae1f53
0x1306a94 <0x1306a94@gmail.com>
2025-08-17 19:18:14 +0800

    添加编辑器QT工程

---
+++ src/editor/src/main.cpp
@@ -0,0 +1,36 @@
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QQuickWindow>
+
+int main(int argc, char *argv[]) {
+
+    QApplication::setApplicationName("SeatCraftEditor");
+    QApplication::setOrganizationName("com.seatcraft");
+
+    QSurfaceFormat defaultFormat = QSurfaceFormat();
+    defaultFormat.setRenderableType(QSurfaceFormat::RenderableType::OpenGL);
+    defaultFormat.setVersion(3, 2);
+    defaultFormat.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(defaultFormat);
+#if (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0))
+    QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
+#else
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
+
+    QApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+    engine.loadFromModule("main", "Main");
+    auto window = static_cast<QQuickWindow *>(engine.rootObjects().at(0));
+#if (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0))
+    window->setPersistentGraphics(true);
+#else
+    window->setPersistentOpenGLContext(true);
+#endif
+    window->setPersistentSceneGraph(true);
+    return app.exec();
+}
